34b06f899c19be691b74d6a5e7bca9d602a62161
Tatsuya Ichikawa <>
2019-01-30 00:12:43 +0900

    [Add] Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'boards', to: 'boards#index'
-  # get 'boards/new', to: 'boards#new'
-  # post 'boards', to: 'boards#create'
-  # get 'boards/:id', to: 'boards#show'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
